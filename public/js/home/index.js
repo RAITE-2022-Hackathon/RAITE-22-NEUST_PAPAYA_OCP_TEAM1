2ec5fc0e7eaca71d16e1f224a779fd236c64c218
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-10-01 15:34:45 +0800

    v5

--- public/js/home/index.js
+++ public/js/home/index.js
@@ -52,7 +52,7 @@ const state = {
     /* [Tag object] */
     // btnKey: document.getElementById("key"),
     // btnLook: document.getElementById("look"),
-    btnNew: document.getElementById("post"),
+    btnNew: document.getElementById("btn-new"),
     Userid: document.getElementById("id"),
     modalTitle: document.getElementById("modal-title"),
     btnEngrave: document.getElementById("engrave"),
@@ -66,8 +66,8 @@ const state = {
         // state.btnKey.disabled = false;
         // state.btnLook.addEventListener("click", state.ask);
         // state.btnLook.disabled = false;
-        state.btnNew.addEventListener("click", state.store);
-        // state.btnNew.disabled = false;
+        state.btnNew.addEventListener("click", state.create);
+        state.btnNew.disabled = false;
 
         state.ask();
     },
@@ -80,33 +80,28 @@ const state = {
     },
     writer: (model) => {
         var index = $("#post-main").length;
-        index =
-            $(`#model-${model.id}`).length == 0
-                ? index
-                : $(`#model-${model.id}`).data("index");
-
+        index+=1;
                 let divrow = $("<tr>", {
                     id: `model-${model.id}`,
-                    "data-index": index,
+                    "data-index": model.id-1,
                     class:"row",
                 });
-                // alert(index);
 
-        let divcard = $("<div>", {id: `model-${model.id}`,"data-index": index,class: "card",style:"width: 50%; position: relative; left:350px"}).appendTo(divrow);
+        let divcard = $("<div>", {id: `model-${model.id}`,class: "card",style:"width: 50%; position: relative; left:350px"}).appendTo(divrow);
         let cardheader = $("<div>", { id: `model-${model.id}`,class: "card-header"});
         let cardheaderform = $("<div>", { id: `model-${model.id}`,class: "card-header-form"});
         let divcardbody = $("<div>", { class: "card-body"});
         $("<h4>", { class:"card-title", html: model.fullName }).appendTo(cardheader);
         $("<p>", { html: model.date +" || "+ model.time }).appendTo(divcardbody);
         $("<p>", { class:"card-text", html: model.description}).appendTo(divcardbody);
-        let votebtn = $("<a>", { class:"btn btn-success btn-find", style:"width:50px;"});
-        $("<i>", { class:"fas fa-ellipsis-v-alt" }).appendTo(votebtn);   
+        let votebtn = $("<a>", { class:"btn btn-success btn-find", "data-index": model.id-1,"data-id": model.id,style:"width:50px;"});
+        $("<i>", { class:"fas fa-pencil-alt" }).appendTo(votebtn);
+        let votebtn1 = $("<a>", { class:"btn btn-danger btn-delete", "data-index": model.id-1,"data-id": model.id,style:"width:50px;"});
+        $("<i>", { class:"fas fa-trash-alt" }).appendTo(votebtn1);
 
-        // if(prescandidate_id.value==''){
-        //     votebtn.appendTo(divcardbody);
-        // }
         
         votebtn.appendTo(cardheaderform);
+        votebtn1.appendTo(cardheaderform);
         cardheaderform.appendTo(cardheader);
         cardheader.appendTo(divcard);
         divcardbody.appendTo(divcard);
@@ -124,12 +119,12 @@ const state = {
         state.activeIndex = i;
         state.btnEngrave.innerHTML = "Update";
 
-        console.log(state.models[0].id);
+        console.log(state.models[i].id);
 
         state.btnEngrave.removeEventListener("click", state.store);
         state.btnEngrave.addEventListener("click", state.update);
-        state.btnEngrave.setAttribute("data-id", state.models[0].id);
-        fetch.showOnModal(state.models[0]);
+        state.btnEngrave.setAttribute("data-id", state.models[i].id);
+        fetch.showOnModal(state.models[i]);
     },
     store: async (e) => {
         e.preventDefault();
@@ -138,7 +133,7 @@ const state = {
         if (model) {
             state.models.push(model);
             state.writer(model);
-            // $("#modal-main").modal("hide");
+            $("#modal-main").modal("hide");
         }
     },
     update: async () => {
@@ -148,10 +143,12 @@ const state = {
 
         if (model) {
             //    console.log(model)
-            state.models[state.activeIndex] = model;
-            fetch.writer(state.entity, model);
+            // alert(state.activeIndex);
+            // state.models[state.activeIndex] = model;
+            // state.writer(model);
 
             $("#modal-main").modal("hide");
+            location.reload();
         }
     },
     destroy: async (i) => {
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,18 +9,23 @@
         </div>
         <div class="section-body">
             <div class="col-lg-12" style="margin-bottom:20px;">
-                <form id="set-Model">
+                <!-- <form id="set-Model">
                     <div class="input-group">
                         <input type="hidden" name="user_id" id="user_id" value="{{\Illuminate\Support\Facades\Auth::user()->id}}">
-                        <input type="text" class="form-control" id="description" name="description" placeholder="Create Post" style="border-radius:20px; height:50px;">
-                        <a href="javascript:void(0)" class="btn btn-success my-2 my-sm-0" id="post" style="font-size:20px; margin-left:10px; "><span class="fa fa-paper-plane"></span></a>
-                    </div>
+                        <input type="text" class="form-control" id="description" name="description" placeholder="Create Post" style="border-radius:20px; height:50px;"> -->
+                        <a href="javascript:void(0)" class="btn btn-success my-2 my-sm-0" id="btn-new" style="font-size:20px; margin-left:10px; "><span class="fa fa-paper-plane"></span></a>
+                    <!-- </div> -->
                 </form>
             </div>
             <!-- <div class="row"> -->
-                <div class="col-lg-12" id="post-main">
+                <table>
+                <tbody id="post-main">
                     
-                </div>
+                </tbody>
+                </table>
+                <!-- <div class="col-lg-12" id="post-main">
+                    
+                </div> -->
             <!-- </div> -->
         </div>
     </section>
